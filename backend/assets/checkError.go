ed68062d309c2d3254d627645b311bb1aafd3097
Vitonces <vitor27melo@gmail.com>
2022-07-11 11:46:27 -0300

    Removing back-end files. New repository is: 'https://github.com/vitor27melo/MAC439-SAS-BackEnd.git'

--- backend/assets/checkError.go
+++
@@ -1,7 +0,0 @@
-package assets
-
-func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
--- backend/model/course.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Course struct {
-	Id_disciplina string `json:"id_disciplina"`
-	Nome          string `json:"nome"`
-	Sigla         string `json:"sigla"`
-}
