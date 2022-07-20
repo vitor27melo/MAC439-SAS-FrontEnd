dbeea171b87c6fc153f7dde50591e9957fd670cd
Vitonces <vitor27melo@gmail.com>
2022-07-19 21:50:42 -0300

    (feat) Preparing netlify

--- lib/aboutGrade.dart
+++ lib/aboutGrade.dart
@@ -9,9 +9,34 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Nota de segurança SAS - USP'),
       ),
-      body: Center(
-        child: Text("Lorem ipsum")
-      ),
+      body:
+        Center(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 350.0,
+            child: Stack(
+              children: <Widget>[
+                Center(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('obrigado_atila.jpg',
+                              color: Colors.white.withOpacity(0.08),
+                              colorBlendMode: BlendMode.modulate
+                          ),
+                        ]
+
+                    )
+                ),
+                Center(
+                    widthFactor: 8,
+                    child: Text("A sua nota de segurança representa o quão segura calculamos ser a sua visita presencial à universidade.")
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'aboutGrade.dart' as aboutGrade;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'SAS - USP'),
     );
   }
 }
@@ -33,20 +34,26 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   bool formVisible = false;
   int _formsIndex = 1;
+  var user = {};
+  var usuarioLogado = false;
 
+  final nameController = TextEditingController();
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
+  final cpfController = TextEditingController();
 
 
   @override
   void dispose() {
+    nameController.dispose();
     usernameController.dispose();
     passwordController.dispose();
+    cpfController.dispose();
     super.dispose();
   }
 
-  void _login() async{
-    var url = Uri.parse('http://localhost:1323/login');
+  void _login() async {
+    var url = Uri.parse('https://sas-mac439.herokuapp.com/login');
     var response = await http.post(
         url,
         headers: {
@@ -58,18 +65,61 @@ class _MyHomePageState extends State<MyHomePage> {
           "password": passwordController.text
         }
     );
-    print(response.body);
+
+    if (response.statusCode == 200) {
+      Navigator.of(context, rootNavigator: true).pop();
+      Map<String, dynamic> user_json = jsonDecode(response.body);
+      setState(() {
+        user["token"] = user_json["token"];
+        user["nome"] = user_json["nome"];
+        usuarioLogado = true;
+      });
+      final snackBar = SnackBar(
+        content: Text('Bem vindo(a), ${user["nome"]}!'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      final snackBar = SnackBar(
+        content: Text(response.body),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
-  void _banana() async{
+  void _register() async {
+    var url = Uri.parse('https://sas-mac439.herokuapp.com/register');
+    var response = await http.post(
+        url,
+        headers: {
+          "Accept": "application/json",
+          "Access-Control_Allow_Origin": "*"
+        },
+        body: {
+          "username": usernameController.text,
+          "name": nameController.text,
+          "password": passwordController.text,
+          "cpf": cpfController.text
+        }
+    );
 
-    var url = Uri.parse('http://localhost:1323/days');
-    var response = await http.get(url, headers: {
-      "Accept": "application/json",
-      "Access-Control_Allow_Origin": "*"
-    });
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
+    if (response.statusCode == 200) {
+      Navigator.of(context, rootNavigator: true).pop();
+      Map<String, dynamic> user_json = jsonDecode(response.body);
+      setState(() {
+        user["token"] = user_json["token"];
+        user["nome"] = user_json["nome"];
+        usuarioLogado = true;
+      });
+      final snackBar = SnackBar(
+        content: Text('Bem vindo(a), ${user["nome"]}!'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      final snackBar = SnackBar(
+        content: Text(response.body),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 
   void _onGoToAboutGradePage() {
@@ -81,28 +131,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var condition = false;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child:
-          condition ?
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Bom dia, Vítor',
-                  style: Theme.of(context).textTheme.headline6
-                ),
-                const SizedBox(height: 25),
-                const Image(
-                  image: NetworkImage('https://cdn.pixabay.com/photo/2020/04/29/08/24/coronavirus-5107804_960_720.png'),
-                  width: 100,
-                ),
-                const SizedBox(height: 25),
-                Row(
+          child:
+          usuarioLogado ?
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                  'Bom dia, ${user["nome"]}!',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headline6
+              ),
+              const SizedBox(height: 25),
+              Image.asset('coronavirus.png', width: 100),
+              const SizedBox(height: 25),
+              Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -112,17 +161,18 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: Row(
                         children: [
                           const Text('Sua'),
-                          TextButton(onPressed: _onGoToAboutGradePage, child: Text('nota de segurança')),
+                          TextButton(onPressed: _onGoToAboutGradePage,
+                              child: Text('nota de segurança')),
                           const Text('hoje é:')
                         ],
                       ),
                     ),
                   ]
-                )
-              ],
-            )
-            :
-            Column(
+              )
+            ],
+          )
+              :
+          Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Row(
@@ -135,19 +185,18 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Row(
                           children: [
                             TextButton(
-                                child: Text('Faça login'),
-                                onPressed: () {
-                                  Navigator.of(context).restorablePush(_loginDialogBuilder);
-                                },
+                              child: Text('Faça login'),
+                              onPressed: () {
+                                Navigator.of(context).restorablePush(
+                                    _loginDialogBuilder);
+                              },
                             ),
                             const Text('ou'),
                             TextButton(
                               child: Text('cadastre-se'),
                               onPressed: () {
-                                setState(() {
-                                  formVisible = true;
-                                  _formsIndex = 2;
-                                });
+                                Navigator.of(context).restorablePush(
+                                    _registerDialogBuilder);
                               },
                             ),
                             const Text('para acessar a plataforma!')
@@ -157,72 +206,139 @@ class _MyHomePageState extends State<MyHomePage> {
                     ]
                 )
               ]
-            )
+          )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _banana,
+      floatingActionButton: usuarioLogado ? FloatingActionButton(
+        onPressed: null,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),
+      ) : null,
     );
   }
 
-  Route<Object?> _loginDialogBuilder(
-      BuildContext context, Object? arguments) {
-        return RawDialogRoute<void>(
-          pageBuilder: (
-              BuildContext context,
-              Animation<double> animation,
-              Animation<double> secondaryAnimation,
-              ) {
-            return SimpleDialog(
-                titleTextStyle: TextStyle(
-                  color: Colors.green,
-                  fontSize: 20
-                ),
-                titlePadding: EdgeInsets.fromLTRB(12.0, 12.0, 0.0, 5.0),
-                contentPadding: EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 16.0),
-                title: Text('Login'),
-                children: <Widget>[
-                  Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextField(
-                        controller: usernameController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'CPF ou e-mail',
-                        ),
+  Route<Object?> _loginDialogBuilder(BuildContext context, Object? arguments) {
+    return RawDialogRoute<void>(
+      pageBuilder: (BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,) {
+        return SimpleDialog(
+            titleTextStyle: TextStyle(
+                color: Colors.green,
+                fontSize: 20
+            ),
+            titlePadding: EdgeInsets.fromLTRB(12.0, 12.0, 0.0, 5.0),
+            contentPadding: EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 16.0),
+            title: Text('Login'),
+            children: <Widget>[
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'CPF ou e-mail',
                       ),
-                      const SizedBox(height: 12),
-                      TextField(
-                        obscureText: true,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        controller: passwordController,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Senha',
-                        ),
+                    ),
+                    const SizedBox(height: 12),
+                    TextField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Senha',
                       ),
-                      const SizedBox(height: 12),
-                      Container(
-                        child: Align(
+                    ),
+                    const SizedBox(height: 12),
+                    Container(
+                      child: Align(
                           alignment: Alignment.topRight,
                           child: ElevatedButton(
                               onPressed: _login,
                               child: Text("Entrar")
                           )
-                        ),
                       ),
-                     ],
-                  ),
+                    ),
+                  ],
                 ),
-              ]
-            );
-          },
+              ),
+            ]
         );
-    }
-}
+      },
+    );
+  }
 
+  Route<Object?> _registerDialogBuilder(BuildContext context, Object? arguments) {
+    return RawDialogRoute<void>(
+      pageBuilder: (BuildContext context,
+          Animation<double> animation,
+          Animation<double> secondaryAnimation,) {
+        return SimpleDialog(
+            titleTextStyle: TextStyle(
+                color: Colors.green,
+                fontSize: 20
+            ),
+            titlePadding: EdgeInsets.fromLTRB(12.0, 12.0, 0.0, 5.0),
+            contentPadding: EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 16.0),
+            title: Text('Cadastro'),
+            children: <Widget>[
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome ou apelido',
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    TextField(
+                      controller: usernameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'E-mail',
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    TextField(
+                      controller: cpfController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'CPF',
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    TextField(
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Senha',
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Container(
+                      child: Align(
+                          alignment: Alignment.topRight,
+                          child: ElevatedButton(
+                              onPressed: _register,
+                              child: Text("Registrar")
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]
+        );
+      },
+    );
+  }
+}
